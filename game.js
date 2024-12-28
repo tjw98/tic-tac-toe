3a719027058e0945c7e7e133f677e5c9919df3ed
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-12-28 13:18:50 -0800

    Finalise console version

--- game.js
+++ game.js
@@ -31,6 +31,7 @@ const game = (function() {
   let gameOver = false;
   let currentPlayer = player1;
 
+  // Converts player input into gameBoard coordinates
   function convert(n) {
     const row = Math.floor((n - 1) / 3); // Compute the row
     const col = (n - 1) % 3;            // Compute the column
@@ -49,6 +50,7 @@ const game = (function() {
       8: [[0, 2], [1, 1], [0 ,1]],
     }
 
+    // Returns winning player if any of the win conditions are met
     for(let condition in winConditions) {
       let positionCounter = 0
       for(let position in winConditions[condition]) {
@@ -57,57 +59,43 @@ const game = (function() {
         if(gameBoard.gameBoard[posY][posX] == player.marker) {
           positionCounter++
         }
-        console.log("position counter: ", positionCounter);
       }
       if(positionCounter == 3) {
         return player
       }
     }
-
- 
-    // WIN CONDITIONS
-    // 10 11 12
-    // 20 21 22
-    // 00 10 20
-    // 01 11 21
-    // 02 12 22
-    // 00 11 22
-    // 02 11 01
-
-    // 1 2 3
-    // 4 5 6
-    // 7 8 9
-    // 1 4 7
-    // 2 5 8
-    // 3 6 9
-    // 1 5 9
-    // 3 5 7
   }
 
   function takeTurn(player) {
-    console.log(`${player.name} turn:`);
     getInput(player);
     return player == player1 ? player2 : player1
   }
 
   function getInput(player) {
-    const input = parseInt(prompt("choose position: 1-9: "));
-    console.log(typeof input, input) // Testing
-    const [inputY, inputX] = convert(input);
-    gameBoard.gameBoard[inputY][inputX] = player.marker;
+    do {
+      const input = parseInt(prompt("choose position: 1-9: "))
+      const [inputY, inputX] = convert(input);
+      
+      if(gameBoard.gameBoard[inputY][inputX] == null) {
+        gameBoard.gameBoard[inputY][inputX] = player.marker;
+        gameBoard.render();
+        break;
+      }
+    }
+    while (true)
   }
 
   function start() {
-    let counter = 0;
     gameBoard.render();
     do {
       currentPlayer = takeTurn(currentPlayer);
-      gameBoard.render();
-      if(checkWin(player1)) { //TURN THIS TO A LOOP OF PLAYER ARRAY
-        alert(`${player1.name} wins!`);
-        gameOver = true;
-      };
-      counter++;
+      console.log(`${currentPlayer.name}'s turn:`);
+      for(let player of players) {
+        if(checkWin(player)) {
+          alert(`${player.name} wins!`);
+          gameOver = true;
+        };
+      }
     }
     while(!gameOver);
   }
