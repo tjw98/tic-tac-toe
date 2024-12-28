b79749a71f19511804ddd613c96fb7a4da226e5c
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-12-27 19:04:58 -0800

    Create Gameboard with render func

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
 const gameBoard = (function() {
-  alert("test");
+  const gameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  const render = function() {
+    for(let row in gameBoard) {
+      console.log(gameBoard[row]);
+    }
+  }
+
+  return {render}
 })()
+
+gameBoard.render();
