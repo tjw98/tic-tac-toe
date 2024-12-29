4fd585d64ee14762b53c3b6b17d6d9683ce58030
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-12-28 17:26:04 -0800

    Update game logic to manipulate DOM vs console

--- game.js
+++ game.js
@@ -1,18 +1,29 @@
 // Create Gameboard
 const gameBoard = (function() {
-  const gameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
+  const cell1 = document.querySelector("[data-id1]");
+  const cell2 = document.querySelector("[data-id2]");
+  const cell3 = document.querySelector("[data-id3]");
+  const cell4 = document.querySelector("[data-id4]");
+  const cell5 = document.querySelector("[data-id5]");
+  const cell6 = document.querySelector("[data-id6]");
+  const cell7 = document.querySelector("[data-id7]");
+  const cell8 = document.querySelector("[data-id8]");
+  const cell9 = document.querySelector("[data-id9]");
+
+  const gameBoardVisual = [
+    cell1, cell2, cell3,
+    cell4, cell5, cell6,
+    cell7, cell8, cell9,
   ];
 
   const render = function() {
-    for(let row in gameBoard) {
-      console.log(gameBoard[row]);
+    for(let pos in gameBoardVisual) {
+      console.log(gameBoardVisual[pos]);
+      gameBoardVisual[pos].textContent = gameBoardVisual[pos];
     }
   }
 
-  return {render, gameBoard}
+  return {render, gameBoardVisual}
 })()
 
 // Create Player constructor
@@ -28,81 +39,73 @@ const players = [player1, player2];
 
 // Create Game Logic
 const game = (function() {
-  let gameOver = false;
   let currentPlayer = player1;
-
-  // Converts player input into gameBoard coordinates
-  function convert(n) {
-    const row = Math.floor((n - 1) / 3); // Compute the row
-    const col = (n - 1) % 3;            // Compute the column
-    return [row, col];
-  }
 
   function checkWin(player) {
     const winConditions = {
-      1: [[0, 0], [0, 1], [0, 2]], // Horizontal top
-      2: [[1, 0], [1, 1], [1, 2]], // Horizontal middle
-      3: [[2, 0], [2, 1], [2, 2]], // Horizontal bottom
-      4: [[0, 0], [1, 0], [2, 0]], // Vertical left
-      5: [[0, 1], [1, 1], [2, 1]], // Vertical middle
-      6: [[0, 2], [1, 2], [2, 2]], // Vertical right
-      7: [[0, 0], [1, 1], [2, 2]], // Diagonal left 
-      8: [[0, 2], [1, 1], [2, 0]], // Diagonal right
+      1: [1, 2, 3], // Horizontal top
+      2: [4, 5, 6], // Horizontal middle
+      3: [7, 8, 9], // Horizontal bottom
+      4: [1, 4, 7], // Vertical left
+      5: [2, 5, 8], // Vertical middle
+      6: [3, 6, 9], // Vertical right
+      7: [1, 5, 9], // Diagonal left 
+      8: [3, 5, 7], // Diagonal right
     }
 
-    // Returns winning player if any of the win conditions are met
+    // Returns winning player if any of the win conditions are met 
+    // Loop through each gameboard cell and record the number of player markers into an array
+    let markerArray = [];
+
+    for(let cell in gameBoard.gameBoardVisual) {
+      if(gameBoard.gameBoardVisual[cell].textContent == player.marker) {
+        markerArray.push(Number(cell) + 1);
+      }
+    }
+    console.log(markerArray) // Testing
+
+    // Compare those array indexes to the win conditions
     for(let condition in winConditions) {
-      let positionCounter = 0
+      let markerCounter = 0;
       for(let position in winConditions[condition]) {
-        const posY = winConditions[condition][position].slice(0, 1);
-        const posX = winConditions[condition][position].slice(1);
-        if(gameBoard.gameBoard[posY][posX] == player.marker) {
-          positionCounter++
+        for(let index in markerArray) {
+          if(markerArray[index] == winConditions[condition][position]) {
+            markerCounter++;
+          }
         }
       }
-      if(positionCounter == 3) {
+      if(markerCounter > 2) {
         return player
       }
     }
   }
+    
 
-  function takeTurn(player) {
-    getInput(player);
+  function switchPlayer(player) {
     return player == player1 ? player2 : player1
   }
 
-  function getInput(player) {
-    do {
-      const input = parseInt(prompt("choose position: 1-9: "))
-      const [inputY, inputX] = convert(input);
-      
-      if(gameBoard.gameBoard[inputY][inputX] == null) {
-        gameBoard.gameBoard[inputY][inputX] = player.marker;
-        gameBoard.render();
-        break;
-      }
-    }
-    while (true)
+  function takeTurn(e) {
+    e.target.textContent = currentPlayer.marker;
+    currentPlayer = switchPlayer(currentPlayer);
+    console.log(`${currentPlayer.name}'s turn:`);
+    e.target.disabled = true;
   }
 
-  function start() {
-    gameBoard.render();
-    do {
-      currentPlayer = takeTurn(currentPlayer);
-      console.log(`${currentPlayer.name}'s turn:`);
-      for(let player of players) {
-        if(checkWin(player)) {
-          alert(`${player.name} wins!`);
-          gameOver = true;
-        }; // TODO: implement logic for ties i.e. if no nulls in gameBoard then it's a tie
-      }
-    }
-    while(!gameOver);
-  }
-
-  return {start}
+  return {takeTurn, checkWin}
 })()
 
 // Program starts
-game.start();
+
+// Event listeners
+for(let cell in gameBoard.gameBoardVisual) {
+  const listener = gameBoard.gameBoardVisual[cell].addEventListener("click", (e) => {
+    game.takeTurn(e)
+    for(let player of players) {
+      if(game.checkWin(player)) {
+        alert(`${player.name} wins!`);
+      }; // TODO: implement logic for ties i.e. if no nulls in gameBoard then it's a tie AND GAMEOVER
+    }
+  })
+};
 
